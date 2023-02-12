6d981c1033499bac72a51bd31a94dd3623d233bd
Asher Tuggle <ashertuggle@gmail.com>
2023-02-12 14:52:45 -0700

    Finished up implementation

--- Assets/Scripts/CardAreaScript.cs
+++ Assets/Scripts/CardAreaScript.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class CardAreaScript : MonoBehaviour
 {
-    [SerializeField] ScrollRect scrollRect;
     [SerializeField] Image cardPrefab;
     [SerializeField] Card[] spawnCards;
 
@@ -15,37 +14,30 @@ public class CardAreaScript : MonoBehaviour
     [SerializeField] Sprite[] clubsSprites = new Sprite[Util.RanksCount];
     [SerializeField] Sprite[] spadesSprites = new Sprite[Util.RanksCount];
 
-    List<Image> cards;
-    float wrapLeftPosition;
-    float wrapRightPosition;
+    HorizontalLayoutGroup layout;
+    RectTransform rect;
+    int cardWidth;
+    float wrapRightX;
+    float wrapLeftX;
 
-    public void OnMoved(Vector2 position)
+    public void OnMoved()
     {
-        // WIP
-        // TODO: Make the viewport exactly 4 cards wide so the math can be fixed
-        Debug.Log($"Moved to {position}");
-        if (position.x > wrapRightPosition)
+        if (rect.offsetMin.x < wrapRightX - layout.padding.left)
         {
-            Debug.Log("Wrapping Right");
-            Image card = cards[0];
-            cards.RemoveAt(0);
-            cards.Add(card);
-            card.transform.SetAsLastSibling();
-            scrollRect.normalizedPosition = new Vector2(position.x - (1f / cards.Count), position.y);
+            rect.GetChild(0).SetAsLastSibling();
+            layout.padding.left += cardWidth;
         }
-        else if (position.x < wrapLeftPosition)
+        else if (rect.offsetMin.x > wrapLeftX - layout.padding.left)
         {
-            Debug.Log("Wrapping Left");
-            // Image card = cards[cards.Count - 1];
-            // cards.RemoveAt(cards.Count - 1);
-            // cards.Insert(0, card);
-            // card.transform.SetAsFirstSibling();
-            // card.transform.localPosition = new Vector3(-1, 0, 0);
+            rect.GetChild(rect.childCount - 1).SetAsFirstSibling();
+            layout.padding.left -= cardWidth;
         }
     }
 
     void Start()
     {
+        layout = GetComponent<HorizontalLayoutGroup>();
+        rect = GetComponent<RectTransform>();
         Dictionary<Suit, Sprite[]> spritesBySuit = new()
         {
             { Suit.Hearts, heartsSprites },
@@ -54,15 +46,17 @@ public class CardAreaScript : MonoBehaviour
             { Suit.Spades, spadesSprites },
         };
 
-        cards = new List<Image>(spawnCards.Length);
         foreach (Card card in spawnCards.OrderBy(card => card.suit).ThenByDescending(card => card.rank))
         {
             Image image = Instantiate(cardPrefab, transform);
-            image.name = $"{card.rank}{card.suit}";
             image.sprite = spritesBySuit[card.suit][(int)card.rank];
-            cards.Add(image);
         }
-        wrapLeftPosition = 3f / cards.Count;
-        wrapRightPosition = 1 - wrapLeftPosition;
+
+        // Storing cardWidth as an int because padding.left is an int
+        cardWidth = (int)cardPrefab.rectTransform.sizeDelta.x;
+        float viewportWidth = GetComponentInParent<ScrollRect>().viewport.rect.width;
+        float maskedWidth = (cardWidth * spawnCards.Length) - viewportWidth;
+        wrapRightX = cardWidth - maskedWidth;
+        wrapLeftX = -cardWidth;
     }
 }
--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -1,5 +1,3 @@
-using System;
-
 public static class Util
 {
     public static readonly int RanksCount = (int)Rank.Ace + 1;

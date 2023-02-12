c156fee3f64e9b31c17ea1d040b27c95015ca99d
Asher Tuggle <ashertuggle@gmail.com>
2023-02-11 21:36:22 -0700

    WIP infinite scroll

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public struct Card
+{
+    public Rank rank;
+    public Suit suit;
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,6 @@
+using System;
+
+public static class Util
+{
+    public static readonly int RanksCount = (int)Rank.Ace + 1;
+}
